b72b27859df881a4b055f58717ef860f4248e62b
ABDELALI AIT TALB <106846356+Raxel01@users.noreply.github.com>
2024-12-31 13:59:59 +0100

    Feat Add Gpt-mini model to enabled models

--- LitellmOpenAi/openweb-ui/populate_db.py
+++ LitellmOpenAi/openweb-ui/populate_db.py
@@ -13,23 +13,27 @@ from sqlalchemy.sql import null
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-email = os.getenv('ADMIN_EMAIL')
-password = pwd_context.hash(os.getenv('ADMIN_PASSWORD'))
+email = os.getenv('OPEN_WEB_UI_ADMIN_EMAIL')
+password = pwd_context.hash(os.getenv('OPEN_WEB_UI_ADMIN_PASSWORD'))
+
 role = os.getenv('ROLE')
 oauth_sub=None
-ADMIN_NAME = os.getenv('ADMIN_NAME')
+
+
+OPEN_WEB_UI_ADMIN_NAME = os.getenv('OPEN_WEB_UI_ADMIN_NAME')
 profile_image_url = os.getenv('DEFAULT_IMG')
+
 model_name = os.getenv('MODEL_NAME')
-embedding_model_name = os.getenv('EMBEDDING_MODEL_NAME')
+gptmini_model_name = os.getenv('GPT_MINI_NAME')
 
 try :
     with get_db() as db:
-            query_user = db.query(Auth).filter(Auth.email==email).first()
-            existing_model = db.query(Model).filter(Model.name == model_name).first()
-            embedding_existence = db.query(Model).filter(Model.name == embedding_model_name).first()
+            Is_admin_exist = db.query(Auth).filter(Auth.email==email).first()
+            Is_gpt_exist = db.query(Model).filter(Model.name == model_name).first()
+            Is_gpt_mini_exist = db.query(Model).filter(Model.name == gptmini_model_name).first()
             
-            if query_user is None:
-                print('Admin User added')
+            if Is_admin_exist is None:
+                print('Admin User added Succesfully')
                 id = str(uuid.uuid4())
                 auth = AuthModel(
                     **{"id": id, "email": email, "password": password, "active": True}
@@ -37,10 +41,11 @@ try :
                 result = Auth(**auth.model_dump())
                 db.add(result)
                 user = Users.insert_new_user(
-                    id, ADMIN_NAME, email, profile_image_url, role, oauth_sub
+                    id, OPEN_WEB_UI_ADMIN_NAME, email, profile_image_url, role, oauth_sub
                 )
-            if existing_model is None:
-                print('Gpt Model added Succesfully')
+            
+            if Is_gpt_exist is None:
+                print('GPT Model added Succesfully')
                 modelInstance = ModelForm(
                     id=model_name,
                     base_model_id=None,
@@ -53,7 +58,22 @@ try :
                     is_active=1
                 )
                 Gptmodel = Models.insert_new_model(modelInstance, id)
-                db.commit()
-                db.refresh(result)
+            
+            if Is_gpt_mini_exist is None:
+                print('GPT Mini Model added Succesfully')
+                mini_modelInstance = ModelForm(
+                    id=gptmini_model_name,
+                    base_model_id=None,
+                    name=gptmini_model_name,
+                    meta={"profile_image_url": "/static/favicon.png",
+                          "description": "", "capabilities": {"vision": True, "citations": True},
+                          "suggestion_prompts": None, "tags": []},
+                    params={},
+                    access_control=None,
+                    is_active=1
+                )
+                Gptminimodel = Models.insert_new_model(mini_modelInstance, id)
+            db.commit()
+            db.refresh(result)
 except Exception as e:
     print(f'Error while inserting data to db: {e}')
