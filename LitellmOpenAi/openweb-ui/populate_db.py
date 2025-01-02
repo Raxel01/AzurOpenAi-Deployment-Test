c33b493df5126c256d2e2a83fb7af5c521110ae3
ABDELALI AIT TALB <106846356+Raxel01@users.noreply.github.com>
2025-01-02 18:22:34 +0100

    Feat : add two Groups controlled by The admin
    chore : increment the retries for open-webUI heathcheck

--- LitellmOpenAi/openweb-ui/populate_db.py
+++ LitellmOpenAi/openweb-ui/populate_db.py
@@ -8,8 +8,31 @@ from open_webui.models.users import UserModel, Users
 from open_webui.models.models import Model, ModelForm, Models
 from passlib.context import CryptContext
 from open_webui.models.auths import AuthModel, Auth
+
+from open_webui.models.groups import *
+
 from sqlalchemy import update
 from sqlalchemy.sql import null
+
+def create_group( owner_id, name: str, description):
+        MyGroupModel = GroupForm(
+                name=name,
+                description =description,
+        )
+        GroupeInserted = Groups.insert_new_group(owner_id, MyGroupModel)
+        updateGroupe = GroupUpdateForm(
+                name=GroupeInserted.name,
+                description=GroupeInserted.description,
+                permissions={ "workspace": { "models": True, "knowledge": True, "prompts": False, "tools": False
+                                    }, "chat": { "file_upload": True, "delete": True, "edit": True, "temporary": True} },
+                user_ids=[],
+                admin_ids=None
+        )
+        Groups.update_group_by_id(GroupeInserted.id, updateGroupe)
+        print('Group added Succesfully')
+        return GroupeInserted.id
+
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -33,7 +56,6 @@ try :
             Is_gpt_mini_exist = db.query(Model).filter(Model.name == gptmini_model_name).first()
             
             if Is_admin_exist is None:
-                print('Admin User added Succesfully')
                 id = str(uuid.uuid4())
                 auth = AuthModel(
                     **{"id": id, "email": email, "password": password, "active": True}
@@ -43,9 +65,12 @@ try :
                 user = Users.insert_new_user(
                     id, OPEN_WEB_UI_ADMIN_NAME, email, profile_image_url, role, oauth_sub
                 )
+                print('Admin User added Succesfully')
+                first_Id  = create_group(id, os.getenv('FIRSTGROUPE'), f"This Group is only for Hr departement they will have access To {model_name}")
+                second_Id = create_group(id, os.getenv('SECGROUPE'), f'This Group is only for Developers They will have access To {gptmini_model_name}')
             
             if Is_gpt_exist is None:
-                print('GPT Model added Succesfully')
+                
                 modelInstance = ModelForm(
                     id=model_name,
                     base_model_id=None,
@@ -54,26 +79,37 @@ try :
                            "description": "", "capabilities": {"vision": True, "citations": True},
                            "suggestion_prompts":None, "tags": []},
                     params={},
-                    access_control=None,
+                    access_control={
+                            "read": {
+                                    "group_ids": [ second_Id],
+                                    "user_ids": []},
+                            "write": { "group_ids": [], "user_ids": []}
+                        },
                     is_active=1
                 )
                 Gptmodel = Models.insert_new_model(modelInstance, id)
+                print('GPT Model added Succesfully')
             
             if Is_gpt_mini_exist is None:
-                print('GPT Mini Model added Succesfully')
                 mini_modelInstance = ModelForm(
                     id=gptmini_model_name,
                     base_model_id=None,
                     name=gptmini_model_name,
                     meta={"profile_image_url": "/static/favicon.png",
                           "description": "", "capabilities": {"vision": True, "citations": True},
-                          "suggestion_prompts": None, "tags": []},
+                          "suggestion_prompts":None, "tags": []},
                     params={},
-                    access_control=None,
+                    access_control={
+                            "read": {
+                                    "group_ids": [first_Id],
+                                    "user_ids": []},
+                            "write": { "group_ids": [], "user_ids": []}
+                        },
                     is_active=1
                 )
                 Gptminimodel = Models.insert_new_model(mini_modelInstance, id)
-            db.commit()
-            db.refresh(result)
+                print('GPT Mini Model added Succesfully')
+                db.commit()
+                db.refresh(result)
 except Exception as e:
     print(f'Error while inserting data to db: {e}')
